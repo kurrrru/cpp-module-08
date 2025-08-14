032c6529a2dfc17fe3c35e8d51111af82bbfcc4d
kurrrru <kurrrru8850@gmail.com>
2025-08-14 15:22:56 +0900

    update

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,6 +2,10 @@
 
 #include <ex02/MutantStack.hpp>
 
+namespace subject {
+    void test();
+}  // namespace subject
+
 int main() {
     MutantStack<int> stack;
     for (int i = 0; i < 20; ++i) {
@@ -29,5 +33,30 @@ int main() {
         std::cout << *rit << " \n"[std::distance(rit, stack.crend()) == 1];
     }
 
+    subject::test();
+
     return 0;
+}
+
+void subject::test() {
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;while (it != ite)
+    {
+    std::cout << *it << std::endl;
+    ++it;
+    }
+    std::stack<int> s(mstack);
 }
